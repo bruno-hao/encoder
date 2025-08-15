b673a9e32e9f3904aea61cae5d07388b876680a9
bruno-hao <brunohao@gmail.com>
2025-08-15 14:02:15 -0300

    Alteração Inicial

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+class Veiculo {
+
+    public $modelo;
+    public $cor;
+    public $ano;
+
+    public function Andar() {
+    echo "Andou<br>";
+    }
+
+    public function Parar() {
+    echo "Parou<br>";
+    }
+}
+
+class Carro extends Veiculo{
+
+
+}
+
+class Moto extends Veiculo{
+
+}
+
+$carro = new Carro();
+
+$carro->modelo = "Gol";
+$carro->cor = "Vermelho";
+$carro->ano = 2018;
+
+$carro->Andar();
+
+var_dump($carro);
+
+$moto = new Moto();
+$moto->modelo = "Honda Biz";
+$moto->cor = "Azul";
+$moto->ano = 2017;
+
+$moto->Parar();
+
+?>
